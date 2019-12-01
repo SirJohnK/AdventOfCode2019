e00c71be7eeefb7dad423328657e43e755c431d4
SirJohnK <SirJohnK@users.noreply.github.com>
2019-12-01 18:29:38 +0100

    Add project files.

---
+++ AdventOfCode2019/Day1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    public static class Day1
+    {
+        public static int ExecutePart1()
+        {
+            return File.ReadAllLines(@"..\..\..\Day1Input.txt").ToList().Sum(mass => ((int)Math.Floor((double)int.Parse(mass) / 3)) - 2);
+        }
+
+        public static int ExecutePart2()
+        {
+            return File.ReadAllLines(@"..\..\..\Day1Input.txt").ToList().Sum(line => Fuel(int.Parse(line)));
+
+            int Fuel(int value, int sum = 0)
+            {
+                var fuel = ((int)Math.Floor((double)value / 3)) - 2;
+                return fuel > 0 ? Fuel(fuel, sum + fuel) : sum;
+            }
+        }
+    }
+}
---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventOfCode2019
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Day 1
+            Console.WriteLine($"Result Day 1 (Part 1): {Day1.ExecutePart1()}");
+            Console.WriteLine($"Result Day 1 (Part 2): {Day1.ExecutePart2()}");
+        }
+    }
+}
