f8f0f73af58637015ca0e8920cdd4071e636a1aa
SirJohnK <SirJohnK@users.noreply.github.com>
2019-12-10 23:43:49 +0100

    Solution for Day 7.

---
+++ AdventOfCode2019/Day7.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    public static class Day7
+    {
+        private static IEnumerable<IEnumerable<T>> Permutate<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return Permutate(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        public static int ExecutePart1()
+        {
+            //Init
+            var result = new List<int>();
+            var phasesettings = Permutate(new List<int>() { 0, 1, 2, 3, 4 }, 5);
+            var program = File.ReadAllText(@"..\..\..\Day7Input.txt").Split(",").Select(value => int.Parse(value)).ToList();
+            var computer = new IntcodeComputer(program);
+
+            foreach (var phasesetting in phasesettings)
+            {
+                var signal = 0;
+                foreach (var ps in phasesetting)
+                {
+                    signal = computer.Run(new Queue<int>(new int[] { ps, signal }));
+                }
+                result.Add(signal);
+            }
+
+            return result.Max();
+        }
+
+        public static int ExecutePart2()
+        {
+            //Init
+            var result = new List<int>();
+            var phasesettings = Permutate(new List<int>() { 5, 6, 7, 8, 9 }, 5);
+            var program = File.ReadAllText(@"..\..\..\Day7Input.txt").Split(",").Select(value => int.Parse(value)).ToList();
+
+            foreach (var phasesetting in phasesettings)
+            {
+                //Init
+                var signal = 0;
+                var computers = new List<IntcodeComputer>()
+                {
+                    new IntcodeComputer(program), //A
+                    new IntcodeComputer(program), //B
+                    new IntcodeComputer(program), //C
+                    new IntcodeComputer(program), //D
+                    new IntcodeComputer(program), //E
+                };
+
+                //While E is not Finished
+                while (computers[4].State != ProgramState.Finished)
+                {
+                    for (int index = 0; index < 5; index++)
+                    {
+                        var input = computers[index].State == ProgramState.Initialized ? new Queue<int>(new int[] { phasesetting.ToList()[index], signal }) : new Queue<int>(new int[] { signal });
+                        signal = computers[index].Run(input, false);
+                    }
+                }
+                result.Add(signal);
+            }
+
+            return result.Max();
+        }
+    }
+}
---
+++ AdventOfCode2019/IntcodeComputer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    public enum ProgramState
+    {
+        Initialized = 0,
+        Running = 1,
+        Paused = 2,
+        Finished = 3,
+    }
+
+    public class IntcodeComputer
+    {
+        private int index = 0;
+        private int output = 0;
+        private List<int> backup;
+        private List<int> program;
+
+        public IntcodeComputer(List<int> program)
+        {
+            //Init
+            backup = program;
+            this.program = backup.ToList();
+            State = ProgramState.Initialized;
+        }
+
+        public ProgramState State { get; private set; }
+
+        public int Run(Queue<int> input, bool reset = true)
+        {
+            //Reset
+            if (reset)
+            {
+                index = 0;
+                program = backup.ToList();
+            }
+
+            //Init
+            output = 0;
+            State = (program[index] == 99 ? ProgramState.Finished : ProgramState.Running);
+
+            //Run program
+            while (State == ProgramState.Running)
+            {
+                //Init
+                var instruction = $"000{program[index]}".Reverse().Take(4).ToList();
+
+                switch (instruction[0])
+                {
+                    case '1': //Add
+                    case '2': //Multiply
+                        {
+                            var firstValue = GetValue(program, instruction[2], index + 1);
+                            var secondValue = GetValue(program, instruction[3], index + 2);
+                            var resultindex = program[index + 3];
+                            program[resultindex] = instruction[0] == '1' ? firstValue + secondValue : firstValue * secondValue;
+                            index += 4;
+                            break;
+                        }
+                    case '3': //Input
+                        {
+                            var data = input.Dequeue();
+                            Console.WriteLine($"Input: {data}");
+                            program[program[index + 1]] = data;
+                            index += 2;
+                            break;
+                        }
+                    case '4': //Output
+                        {
+                            output = GetValue(program, instruction[2], index + 1);
+                            Console.WriteLine($"Output: {output}");
+                            index += 2;
+                            State = ProgramState.Paused;
+                            break;
+                        }
+                    case '5': //Jump-If-True
+                    case '6': //Jump-If-False
+                        {
+                            var value = GetValue(program, instruction[2], index + 1);
+                            var jump = (instruction[0] == '5' && value > 0) || (instruction[0] == '6' && value == 0);
+                            index = jump ? GetValue(program, instruction[3], index + 2) : index += 3;
+                            break;
+                        }
+                    case '7': //Less Than
+                    case '8': //Equals
+                        {
+                            var firstValue = GetValue(program, instruction[2], index + 1);
+                            var secondValue = GetValue(program, instruction[3], index + 2);
+                            var SetBool = (instruction[0] == '7' && firstValue < secondValue) || (instruction[0] == '8' && firstValue == secondValue);
+                            var resultindex = program[index + 3];
+                            program[resultindex] = SetBool ? 1 : 0;
+                            index += 4;
+                            break;
+                        }
+                }
+
+                //Set State
+                State = (program[index] == 99 ? ProgramState.Finished : State);
+            }
+
+            //Return output
+            return output == 0 && input.Count > 0 ? input.Dequeue() : output;
+
+            int GetValue(List<int> program, char mode, int pos) => mode == '0' ? program[program[pos]] : program[pos];
+        }
+    }
+}
